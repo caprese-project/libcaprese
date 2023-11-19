836f33d7b2ede1852d8f0115016add9d0723b665
cosocaf <cosocaf@gmail.com>
2023-11-19 17:08:02 +0900

    wip

---
+++ include/libcaprese/root_boot_info.h
@@ -0,0 +1,23 @@
+#ifndef LIBCAPRESE_ROOT_BOOT_INFO_H_
+#define LIBCAPRESE_ROOT_BOOT_INFO_H_
+
+#include <libcaprese/cap.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+  typedef struct {
+    task_cap_t       root_task_cap;
+    cap_space_cap_t  first_cap_space_cap;
+    page_table_cap_t root_page_table_cap;
+    page_table_cap_t cap_space_page_table_caps[3];
+    mem_cap_t        mem_cap_start;
+    mem_cap_t        mem_cap_end;
+  } root_boot_info_t;
+
+#ifdef __cplusplus
+} // extern "C"
+#endif // __cplusplus
+
+#endif // LIBCAPRESE_ROOT_BOOT_INFO_H_
