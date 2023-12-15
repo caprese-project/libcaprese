83de518f9969518871fee9dffe381198bab56980
cosocaf <cosocaf@gmail.com>
2023-12-16 08:52:36 +0900

    msg buf utils

--- include/libcaprese/ipc.h
+++ include/libcaprese/ipc.h
@@ -1,6 +1,7 @@
 #ifndef LIBCAPRESE_IPC_H_
 #define LIBCAPRESE_IPC_H_
 
+#include <libcaprese/cap.h>
 #include <stddef.h>
 #include <stdint.h>
 
@@ -13,6 +14,14 @@ extern "C" {
     size_t    data_part_length;
     uintptr_t data[126];
   } message_buffer_t;
+
+  static inline uintptr_t msg_buf_delegate(cap_t cap) {
+    return (uintptr_t)cap | ((uintptr_t)1 << (sizeof(uintptr_t) * 8 - 1));
+  }
+
+  static inline uintptr_t msg_buf_transfer(cap_t cap) {
+    return (uintptr_t)cap;
+  }
 
 #ifdef __cplusplus
 } // extern "C"
