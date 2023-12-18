d0416e158033151c649a20dfb7a317d937857f19
cosocaf <cosocaf@gmail.com>
2023-12-18 11:38:22 +0900

    dtb vp cap

--- include/libcaprese/arch/rv64/root_boot_info_rv64.h
+++ include/libcaprese/arch/rv64/root_boot_info_rv64.h
@@ -1,6 +1,7 @@
 #ifndef LIBCAPRESE_ARCH_RV64_ROOT_BOOT_INFO_RV64_H_
 #define LIBCAPRESE_ARCH_RV64_ROOT_BOOT_INFO_RV64_H_
 
+#include <libcaprese/cap.h>
 #include <stdint.h>
 
 #ifdef __cplusplus
@@ -8,8 +9,9 @@ extern "C" {
 #endif // __cplusplus
 
   struct arch_root_boot_info {
-    uintptr_t dtb_start;
-    uintptr_t dtb_end;
+    uintptr_t       dtb_start;
+    uintptr_t       dtb_end;
+    virt_page_cap_t dtb_virt_page_cap;
   };
 
 #ifdef __cplusplus
